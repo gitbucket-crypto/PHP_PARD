bbf82c125add535cfc735adddf74f57c69ea1b62
gitbucket-crypto <69725176+gitbucket-crypto@users.noreply.github.com>
2023-12-18 18:29:36 -0300

    Add files via upload

---
+++ types.php
@@ -0,0 +1,20 @@
+<?php 
+define('DOOR_TYPE','01');
+define('WATER_TYPE','02');
+define('LIGHT_TYPE', '03');
+define('TEMP_HUMIDITY_TYPE','04');
+define('TEMP_TYPE','05');
+define('G_SENSOR_TYPE','06');
+define('RAP_TYPE','07');
+define('GPIO_TYPE','08');
+define('RELAY_TYPE','09');
+define('VOLTAGE_TYPE','0a');  //0a
+define('NOISE_TYPE','0b');  //0b
+define('SMOK_TYPE', '0c');  //0c
+define('ELECTRIC_METER_TYPE', '0d'); //0d
+define('EXT_TEMP_TYPE', '0e');  //0e
+define('EXT_RAP_TYPE','0f');  //0f
+define('RTC_TYPE', '10');  //hex 10
+define('HEADER',"a1a2a3");
+define('FOOTER',"a3a2a1");
+?>
